b292d46d1c679cccc5e904a0ae3f4a63b2e4a79c
Connor_Crump <ccrump@uoregon.edu>
2022-12-14 13:37:58 -0800

    completed cart functionality on mobile

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ function closeMenu(){
     document.getElementsByClassName("overlay-menu")[0].style.display = "none";
     document.body.classList.remove("darken");
 }
+/* function to toggle the cart summary overlay */
+function toggleCartSummary() {
+    const cartSummary = document.getElementsByClassName("cart-summary")[0];
+    if (cartSummary.style.display == 'block'){
+        cartSummary.style.display = 'none';
+    }else {
+        cartSummary.style.display = 'block';
+    }
+}
 
 /* photo slider functions and variables*/
 let slideIndex = 1;
@@ -33,11 +42,43 @@ function changeAddToCartCounter(n) {
     document.getElementById("add-to-cart-count").innerText = addToCartCounter;
 }
 
+/* function and variable to update the number of items in the cart*/
+let cartCounter = 0;
+function updateCartCounter(n){
+    cartCounter += n;
+    const emptyMessage = document.getElementById("cart-empty-message");
+    const cartFilled = document.getElementById("cart-filled");
+    const cartFilledNotification = document.getElementById("cart-filled-notification")
+    const counters = document.getElementsByClassName("cart-count");
+    const cartTotal = document.getElementById("cart-total");
+    if (cartCounter > 0){
+        emptyMessage.style.display = "none";
+        cartFilled.style.display = "block";
+        cartFilledNotification.style.display = "block";
+        for (let counter of counters) {
+            counter.textContent = `${cartCounter}`;
+        };
+        cartTotal.textContent = `$${(Number(cartCounter)*125).toFixed(2)}`;
+    }else {
+        emptyMessage.style.display = "block";
+        cartFilled.style.display = "none";
+        cartFilledNotification.style.display = "none";
+    }
+}
+
+/* handler for the add to cart button */
+function addToCartHandler() {
+    const addToCartCount = document.getElementById("add-to-cart-count");
+    updateCartCounter(Number(addToCartCount.textContent));
+    addToCartCount.textContent = 0;
+    addToCartCounter = 0;
+}
+
 /* functions to execute after page load */
 window.addEventListener('load', function () {
     /* set original photo on slider */
     showSlide(slideIndex);
 
-    /* set original cart count to 0 */
+    /* set original add to cart count to 0 */
     changeAddToCartCounter(0);
 });
